47d66acf50fe3b38e99a8f3a74340c3e5e3269e6
Dmitriev Igor <gasik04@rambler.ru>
2017-04-04 21:52:42 +0300

    M commit_2

--- mox/js/common.js
+++ mox/js/common.js
@@ -1,5 +1,26 @@
 $(function() {
 
+	//$(".search-form").click(function(){
+	//	$(this).toggleClass("reset-form")
+	//});
+
+
+
+	$('.registration > li > a').click(function(){
+		var $this = $(this),
+		$pop = $this.next('.popup-form'),
+		$close = $pop.children('.close');
+
+		$this.parent().siblings().children('.popup-form').hide()
+		$pop.show();
+		$close.click(function(){
+			$pop.hide();
+		});
+		return false;
+	});
+
+
+
 	// Кнопка наверх
 	$(".btn-top").click(function() {
 		$("html, body").animate({ scrollTop: 0 }, "slow");
